1908ec20be5cbf566e5d42ca673165454651e696
Jason Hang <jason.d.hang@gmail.com>
2014-09-26 13:14:05 -0400

    Quiz 1 Submitted

---
+++ quiz1.rb
@@ -0,0 +1,11 @@
+# 1. a = 1
+# 2. Arrays are organized in order by indexes, Hashes are organized with key-value pairs
+# 3. arr = [1,2,3,3] returns [1,2,3,3]
+  # [1,2,3,3].uniq returns [1,2,3]
+  # [1,2,3,3].uniq! returns [1,2,3]
+# 4. map goes through each value within the Array, same .each method, and perform the task within the do/end block. Used for iterating each value within the Array.
+  # select will get certain values within the Array based on the given condition within the do/end block. Used for selecting specific values within the Array.
+# 5. hash = {"String" => "Value", "String2" => 1}
+# 6. "no way!"
+# 7. x = 4
+# 8. Error, x is out of scope and cannot be accessed
